0c4979ca0e5e2f6b68c5dc380915b98c4b75ccc7
Filip Copija <filip.copija1@gmail.com>
2023-03-01 13:21:22 +0100

    Create Data folder, Add AppDbContext, DbSet

---
+++ GameStore/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GameStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
