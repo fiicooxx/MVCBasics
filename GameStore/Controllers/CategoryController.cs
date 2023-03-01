a138ffb5149ec18f3dd4422e3baedb2faeaeb5b5
Filip Copija <filip.copija1@gmail.com>
2023-03-01 17:28:05 +0100

    Retrive categories

---
+++ GameStore/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using GameStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _appDbContext.Categories.ToList();
+            return View();
+        }
+    }
+}
