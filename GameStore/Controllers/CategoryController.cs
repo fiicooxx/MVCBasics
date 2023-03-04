b9a59930097b113d7c11335f7bf51634bff6d6b1
Filip Copija <filip.copija1@gmail.com>
2023-03-04 12:19:03 +0100

    Get Edit

--- GameStore/Controllers/CategoryController.cs
+++ GameStore/Controllers/CategoryController.cs
@@ -42,5 +42,37 @@ namespace GameStore.Controllers
             }
             return View(category);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var categoryFromDb = _appDbContext.Categories.Find(id);
+            //var categoryFromDbFirst = _appDbContext.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDbSingle = _appDbContext.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null) return NotFound();
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Categories.Add(category);
+                _appDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
